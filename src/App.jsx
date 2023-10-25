dfe6e73aff1007bfbb5e196f39f60931138952b4
Swapnil Singh <swapnil.singh@razorpay.com>
2023-10-25 11:26:17 +0530

    Added fingerprint js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { UAParser } from 'ua-parser-js';
 
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import { useState } from 'react';
+
+const fpPromise = FingerprintJS.load();
+
 function App() {
   const {browser, device, os} = UAParser(navigator.userAgent);
+  const [userId, setUserId] = useState(null);
+  
+  (async () => {
+    // Get the visitor identifier when you need it.
+    const fp = await fpPromise
+    const result = await fp.get()
+    console.log(result);
+    setUserId(result.visitorId);
+  })();
+
   return (
     <>
-       <h2>Device Token POC</h2>
+      <h2>Device Token POC</h2>
+      <h4>Visitor ID: {userId}</h4>
       <div className="card">
       <p>
         Browser 
